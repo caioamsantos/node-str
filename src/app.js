0e23a2896dc3b6a704f502431782b6d2a21baa26
caioamsantos <54689983+caioamsantos@users.noreply.github.com>
2020-04-21 20:53:10 -0300

    criação de rotas

---
+++ src/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+const indexRoute = require('../src/routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
