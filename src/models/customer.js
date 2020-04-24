5df128c989bb27957f7ef445dc031ef6e9300733
caioamsantos <54689983+caioamsantos@users.noreply.github.com>
2020-04-24 20:01:27 -0300

    inclusão de roles

--- src/models/customer.js
+++ src/models/customer.js
@@ -4,20 +4,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    name:{
+    name: {
         type: String,
         required: true,
         trim: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    password:{
+    password: {
         type: String,
         required: true,
-    }
+    },
+    roles: [{
+        type: String,
+        require: true,
+        enum: ['user', 'admin'],
+        default: ['user']
+    }]
 });
 
 module.exports = mongoose.model('Customer', schema);
