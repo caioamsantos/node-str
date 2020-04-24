6179fe6d4e691e51c9019a4a00bb1372c57d73fa
caioamsantos <54689983+caioamsantos@users.noreply.github.com>
2020-04-24 19:55:31 -0300

    inclusão de rotas, cors e connection string

--- src/app.js
+++ src/app.js
@@ -4,21 +4,35 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-
+const config = require('./config');
 const app = express();
 
-mongoose.connect('mongodb+srv://ca9:ca9@cluster0-5e9bp.mongodb.net/nodestr?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect(config.connectionString, { useNewUrlParser: true });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
 
 const indexRoute = require('../src/routes/index-route');
 const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
+const orderRoute = require('./routes/order-route');
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
 
 module.exports = app;
