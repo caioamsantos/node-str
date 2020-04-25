959f54c5122b37d67846a911604b58c70408746c
caioamsantos <54689983+caioamsantos@users.noreply.github.com>
2020-04-25 15:36:48 -0300

    alteração de variável de ambiente do mongo

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ global.SALT_KEY = process.env.SALT_KEY;
 global.EMAIL_TMPL = process.env.EMAIL_TMPL;
 
 module.exports = {
-    connectionString: process.env.CONNECTION_STRING,
+    connectionString: process.env.MONGODB_URI,
     sendgridKey: process.env.SENDGRID_KEY,
 }
